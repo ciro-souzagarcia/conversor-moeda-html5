a32d165841432eacac9d95f210efdb051dd0cfc3
Ciro Souza Garcia <ciro@kayrus.com.br>
2019-08-24 01:27:15 +0000

    empacotando funções

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
-function chamarAPI(){
-	console.log("Chamado API");
-	const endpoint = "api/latest";
-	var oParametros = { 
-		base: $("#moeda_origem").val()
-	};
-	$.get(endpoint, oParametros, callback );
-	
-	
-}
-
-function callback(resultado){
-	console.log(resultado);
-	var sMoedaDestino = $("#moeda_destino").val();
-	var fvalor = resultado.rates[sMoedaDestino];
-	
-	$("#valor_destino2").text(fvalor);
-	$("#moeda_destino2").text(sMoedaDestino);
+const ovly = { };
+ovly.conversor = {
+	versao: "1", 
+	chamarAPI: function(){
+		console.log("Chamado API");
+		const endpoint = "api/latest";
+		var oParametros = { 
+			base: $("#moeda_origem").val()
+		};
+		$.get(endpoint, oParametros, this.callback );
+	},
+	callback: function(resultado){
+		// console.log(resultado);
+		var sMoedaDestino = $("#moeda_destino").val();
+		var fvalor = resultado.rates[sMoedaDestino];
+		
+		$("#valor_destino2").text(fvalor);
+		$("#moeda_destino2").text(sMoedaDestino);
+	}
 }
